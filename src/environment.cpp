5f1b7d434c2b0062c99e052c838718dc8b26f949
shimshimlee <shimshimlee@gmail.com>
2020-03-02 23:42:51 +0900

    0302

--- src/environment.cpp
+++ src/environment.cpp
@@ -58,6 +58,22 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     renderPointCloud(viewer,segmentCloud.first,"obstCloud",Color(1,0,0));
     renderPointCloud(viewer,segmentCloud.second,"planeCloud",Color(0,1,0));
 
+    std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> cloudClusters = pointProcessor.Clustering(segmentCloud.first, 1.0, 3, 30);
+
+    int clusterId = 0;
+    std::vector<Color> colors = {Color(1,0,0), Color(1,1,0), Color(0,0,1)};
+    for(pcl::PointCloud<pcl::PointXYZ>::Ptr cluster :cloudClusters)
+    {
+        std::cout << "cluster size";
+        pointProcessor.numPoints(cluster);
+        renderPointCloud(viewer, cluster, "obsCloud"+std::to_string(clusterId),colors[clusterId%colors.size()]);
+
+        //Box box = pointProcessor.BoundingBox(cluster);
+        //renderBox(viewer,box,clusterId);
+
+        ++clusterId;
+    }
+
 }
 
 
